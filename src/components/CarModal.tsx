06854d06934ebf0c07963888fe2c8a4d1519635b
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2024-12-31 08:40:52 -0300

    feat: html from CartModal created

--- src/components/CarModal.tsx
+++
@@ -1,3 +0,0 @@
-const CartModal = () => {};
-
-export default CartModal;
---
+++ src/components/CartModal.tsx
@@ -0,0 +1,18 @@
+const CartModal = () => {
+  return (
+    <div>
+      <div>
+        <h1>Carrinho de compras</h1>
+        <button>X</button>
+      </div>
+      <div></div>
+      <div>
+        <p>Total</p>
+        <p>R$ 0,00</p>
+      </div>
+      <button>Remover todos</button>
+    </div>
+  );
+};
+
+export default CartModal;
